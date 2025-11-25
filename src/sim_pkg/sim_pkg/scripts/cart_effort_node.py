f601934cc512b9f600393076ecd6889672f34977
pekachoo <jliu6162@gmail.com>
2025-11-24 23:29:28 -0500

    randomized efforts for testing

--- src/sim_pkg/sim_pkg/scripts/cart_effort_node.py
+++ src/sim_pkg/sim_pkg/scripts/cart_effort_node.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 import rclpy
+import random
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
 
 class CartEffortPublisher(Node):
     def __init__(self):
         super().__init__('cart_effort_pub')
-
-        self.force_value = -1000.0
         
         self.joint_name = "slider_to_cart"
 
         self.pub = self.create_publisher(JointState, 'cart_effort', 10)
 
-        self.timer = self.create_timer(0.02, self.publish_effort)
+        self.timer = self.create_timer(0.7, self.publish_effort)
 
     def publish_effort(self):
+        effort_value = random.uniform(-20.0, 20.0)
         msg = JointState()
         msg.name = [self.joint_name]
         msg.position = []  
         msg.velocity = []  
-        msg.effort = [self.force_value]
+        msg.effort = [effort_value]
 
         self.pub.publish(msg)
 
